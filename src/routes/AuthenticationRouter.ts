a5dd7def24cdefcd60b59eb30e6811fcc099bdd5
calinortan <calinortan@gmail.com>
2017-03-25 21:09:57 +0200

    switched db to mongolab and fixed token generator on sign in

--- src/routes/AuthenticationRouter.ts
+++ src/routes/AuthenticationRouter.ts
@@ -26,7 +26,7 @@ export class AuthenticationRouter {
   }
 
   public attemptLogin(req: Request, res: Response, next: NextFunction) {
-    const token = JwtTokenCreator.generateToken(req.user.password, Date.now())
+    const token = JwtTokenCreator.generateToken(req.user.id, Date.now())
     res.status(200).json({ status: "succesful", token });
   }
 
--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,17 @@ import mongoose = require('mongoose');
 import DevspaceUtils = require('./utils/DevspaceUtils'); // export = bla bla
 import App from './App'; // export default
 import io = require('socket.io');
+const config = require('../config');
 
 /**
  * @todo Implement a database acces layer
  */
 mongoose.Promise = Promise
-mongoose.connect('mongodb://localhost:auth/devspace');
+// mongoose.connect('mongodb://localhost:auth/devspace');
+mongoose.connect(`mongodb://${config.mongoLabAuth}@ds035016.mlab.com:35016/calinortandb`).catch((error) => {
+  console.log(error);
+});
+
 
 debug('ts-express:server');
 const port = DevspaceUtils.normalizePort(process.env.PORT || 3000);
